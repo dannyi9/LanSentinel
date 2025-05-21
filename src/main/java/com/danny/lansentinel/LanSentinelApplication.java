5e08b8a5263dd119f6d2d6975623291d663d6ae1
dannyi9 <danyaalislam@yahoo.co.uk>
2025-05-21 18:24:27 +0100

    Initial commit

---
+++ src/main/java/com/danny/lansentinel/LanSentinelApplication.java
@@ -0,0 +1,13 @@
+package com.danny.lansentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanSentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanSentinelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danny/lansentinel/LanSentinelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danny.lansentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanSentinelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
