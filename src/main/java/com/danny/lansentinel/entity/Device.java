24f75ce3346ade3c5ec977b421313e0ca7412df5
dannyi9 <danyaalislam@yahoo.co.uk>
2025-05-21 18:30:58 +0100

    Created the Device entity

--- src/main/java/com/danny/lansentinel/entity/Device.java
+++ src/main/java/com/danny/lansentinel/entity/Device.java
@@ -1,4 +1,34 @@
 package com.danny.lansentinel.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
 public class Device {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String hostname;
+    private String ipAddress;
+    private String macAddress;
+    private String vendor;
+
+    private String operatingSystem;
+
+    private Integer openPortCount;
+    private List<Integer> openPorts;
+
+    private Boolean isOnline;
+    private LocalDateTime lastSeen;
+
+    private Boolean isTrusted;
+
+    private String notes;
 }
