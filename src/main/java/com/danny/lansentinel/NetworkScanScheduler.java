dc2725fd017da105553c92367ae0551544631540
dannyi9 <danyaalislam@yahoo.co.uk>
2025-05-23 19:45:44 +0100

    Added a property for the subnet of which to scan

--- src/main/java/com/danny/lansentinel/NetworkScanScheduler.java
+++ src/main/java/com/danny/lansentinel/NetworkScanScheduler.java
@@ -1,5 +1,6 @@
 package com.danny.lansentinel;
 
+import com.danny.lansentinel.config.ScanProperties;
 import com.danny.lansentinel.service.NetworkScannerService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -8,13 +9,15 @@ import org.springframework.stereotype.Component;
 public class NetworkScanScheduler {
 
     private final NetworkScannerService networkScannerService;
+    private final ScanProperties scanProperties;
 
-    public NetworkScanScheduler(NetworkScannerService networkScannerService) {
+    public NetworkScanScheduler(NetworkScannerService networkScannerService, ScanProperties scanProperties) {
         this.networkScannerService = networkScannerService;
+        this.scanProperties = scanProperties;
     }
 
     @Scheduled(fixedDelayString = "#{@scanProperties.scanIntervalMs}")
     public void scheduleNetworkScan() {
-        networkScannerService.scanAndSaveDevices("192.168.100.0/24");
+        networkScannerService.scanAndSaveDevices(scanProperties.getSubnetToScan());
     }
 }
--- src/main/java/com/danny/lansentinel/config/ScanProperties.java
+++ src/main/java/com/danny/lansentinel/config/ScanProperties.java
@@ -7,12 +7,21 @@ import org.springframework.stereotype.Component;
 @ConfigurationProperties(prefix = "lansentinel")
 public class ScanProperties {
     private long scanIntervalMs;
+    private String subnetToScan;
 
     public long getScanIntervalMs() {
         return scanIntervalMs;
     }
 
+    public String getSubnetToScan() {
+        return subnetToScan;
+    }
+
     public void setScanIntervalMs(long scanIntervalMs) {
         this.scanIntervalMs = scanIntervalMs;
+    }
+
+    public void setSubnetToScan(String subnetToScan) {
+        this.subnetToScan = subnetToScan;
     }
 }
