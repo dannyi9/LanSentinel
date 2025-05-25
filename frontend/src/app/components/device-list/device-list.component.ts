c778d98d7b6b0e9beee574055fcea2a6408fde73
dannyi9 <194951921+dannyi9@users.noreply.github.com>
2025-05-25 11:45:34 +0100

    Changed deprecated subscribe in favour of passing in a single object

--- frontend/src/app/components/device-list/device-list.component.ts
+++ frontend/src/app/components/device-list/device-list.component.ts
@@ -55,15 +55,15 @@ export class DeviceListComponent implements OnInit {
 
     console.log('Setting device ID', device.id, ' as trusted:', isTrusted);
 
-    this.deviceService.setTrustedDeviceById(device.id, isTrusted).subscribe(
-      (updatedDevice: Device) => {
+    this.deviceService.setTrustedDeviceById(device.id, isTrusted).subscribe({
+      next: (updatedDevice: Device) => {
         console.log('Device trusted status updated:', updatedDevice);
         device.trusted = updatedDevice.trusted;
       },
-      (error) => {
+      error: (error) => {
         console.error('Error updating trusted status:', error);
       }
-    );
+    });
   }
 
 }
