2d5e3fb4af5416c9bcc81fdc27c07228a4cd3076
dannyi9 <194951921+dannyi9@users.noreply.github.com>
2025-05-25 13:25:40 +0100

    Changed the way tasks are scheduled, added a get scan interval api

--- backend/src/main/java/com/danny/lansentinel/controller/ScanController.java
+++ backend/src/main/java/com/danny/lansentinel/controller/ScanController.java
@@ -1,5 +1,6 @@
 package com.danny.lansentinel.controller;
 
+import com.danny.lansentinel.scheduler.NetworkScanScheduler;
 import com.danny.lansentinel.service.NetworkScannerService;
 
 import org.springframework.http.ResponseEntity;
@@ -10,9 +11,12 @@ import org.springframework.web.bind.annotation.*;
 public class ScanController {
 
     NetworkScannerService networkScannerService;
+    NetworkScanScheduler networkScanScheduler;
 
-    public ScanController(NetworkScannerService networkScannerService) {
+    public ScanController(NetworkScannerService networkScannerService,
+                          NetworkScanScheduler networkScanScheduler) {
         this.networkScannerService = networkScannerService;
+        this.networkScanScheduler = networkScanScheduler;
     }
 
     @GetMapping("/enabled")
@@ -23,6 +27,11 @@ public class ScanController {
     @GetMapping("/enable/{enabled}")
     public ResponseEntity<Boolean> enableScan(@PathVariable Boolean enabled) {
         return ResponseEntity.ok(networkScannerService.setScanEnabled(enabled));
+    }
+
+    @GetMapping("/interval")
+    public ResponseEntity<Long> getScanInterval() {
+        return ResponseEntity.ok(networkScanScheduler.getScanIntervalMs());
     }
 
 }
--- backend/src/main/java/com/danny/lansentinel/scheduler/NetworkScanScheduler.java
+++ backend/src/main/java/com/danny/lansentinel/scheduler/NetworkScanScheduler.java
@@ -2,8 +2,11 @@ package com.danny.lansentinel.scheduler;
 
 import com.danny.lansentinel.config.ScanProperties;
 import com.danny.lansentinel.service.NetworkScannerService;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.concurrent.ScheduledFuture;
 
 @Component
 public class NetworkScanScheduler {
@@ -11,13 +14,38 @@ public class NetworkScanScheduler {
     private final NetworkScannerService networkScannerService;
     private final ScanProperties scanProperties;
 
-    public NetworkScanScheduler(NetworkScannerService networkScannerService, ScanProperties scanProperties) {
+    private final ThreadPoolTaskScheduler taskScheduler;
+    private ScheduledFuture<?> scheduledFuture;
+
+    private long scanIntervalMs;
+
+    public NetworkScanScheduler(NetworkScannerService networkScannerService,
+                                ScanProperties scanProperties,
+                                ThreadPoolTaskScheduler threadPoolTaskScheduler) {
         this.networkScannerService = networkScannerService;
         this.scanProperties = scanProperties;
+        this.taskScheduler = threadPoolTaskScheduler;
+        this.scanIntervalMs = scanProperties.getScanIntervalMs();
+        scheduleTask();
     }
 
-    @Scheduled(fixedDelayString = "#{@scanProperties.scanIntervalMs}")
-    public void scheduleNetworkScan() {
-        networkScannerService.scanAndSaveDevices(scanProperties.getSubnetToScan());
+    private void scheduleTask() {
+        System.out.println("Scheduling network scan task");
+        scheduledFuture = taskScheduler.scheduleWithFixedDelay(
+                () -> networkScannerService.scanAndSaveDevices(scanProperties.getSubnetToScan()),
+                Duration.ofMillis(scanIntervalMs)
+        );
+    }
+
+    public void updateScanInterval(long newIntervalMs) {
+        this.scanIntervalMs = newIntervalMs;
+        if (scheduledFuture != null) {
+            scheduledFuture.cancel(false);
+        }
+        scheduleTask();
+    }
+
+    public long getScanIntervalMs() {
+        return scanIntervalMs;
     }
 }
