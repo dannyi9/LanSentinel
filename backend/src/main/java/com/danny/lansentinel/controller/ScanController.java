375dfbe58b7d00111e615bfe3ffc4ff6bd1c9b77
dannyi9 <194951921+dannyi9@users.noreply.github.com>
2025-05-25 12:54:20 +0100

    Removed unused imports

--- backend/src/main/java/com/danny/lansentinel/controller/ScanController.java
+++ backend/src/main/java/com/danny/lansentinel/controller/ScanController.java
@@ -2,12 +2,8 @@ package com.danny.lansentinel.controller;
 
 import com.danny.lansentinel.service.NetworkScannerService;
 
-import com.danny.lansentinel.entity.Device;
-import com.danny.lansentinel.repository.DeviceRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 public class ScanController {
