cf1922a291a363feb73a87ff0ca906470bb11395
dannyi9 <194951921+dannyi9@users.noreply.github.com>
2025-06-24 18:57:51 +0100

    Added logging

--- backend/src/main/java/com/danny/lansentinel/LanSentinelApplication.java
+++ backend/src/main/java/com/danny/lansentinel/LanSentinelApplication.java
@@ -1,5 +1,7 @@
 package com.danny.lansentinel;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -8,7 +10,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class LanSentinelApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(LanSentinelApplication.class);
+
     public static void main(String[] args) {
+        logger.info("LanSentinel starting...");
         SpringApplication.run(LanSentinelApplication.class, args);
     }
 
